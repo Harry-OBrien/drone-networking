1b478175a5d976205bd31e2272eac54d7a5636e4
a64070ho <harry.obrien@student.manchester.ac.uk>
2020-01-05 00:18:05 +0000

    First init

---
+++ SwarmListener/SwarmListener/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SwarmListener
+//
+//  Created by Harry O'Brien on 05/01/2020.
+//  Copyright © 2020 SociaDevLtd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
